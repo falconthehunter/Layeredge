9404eac702446f5742f40800933615e1ba05cd5a
Falcon <163686276+falconthehunter@users.noreply.github.com>
2025-01-27 17:31:46 +0600

    Add files via upload

---
+++ utils/banner.js
@@ -0,0 +1,13 @@
+const banner = `
+
+    === Running LayerEdge CLI Version ===
+** ====================================== **
+*    This script is created for free use   *
+*  Do not sell or distribute it for profit *
+** ====================================== **
+
+
+* Author: @falcon                
+* Github Link: github.com/falconthehunter         
+                 `
+export default banner;
